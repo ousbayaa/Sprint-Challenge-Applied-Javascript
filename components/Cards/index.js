b5d2d546c43fba30b7d43248ff2fb90667396cfe
Ous Bayaa <bayaaous@gmail.com>
2020-02-14 09:16:41 -0800

    created elements for index cards

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,39 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+    .then(response => {
+        console.log(response);
+    });
+
+function createCard(data) {
+    const card = document.createElement('div');
+    const headLine = document.createElement('div');
+    const author = document.createElement('div');
+    const imgCont = document.createElement('div');
+    const img = document.createElement('img');
+    const authorName = document.createElement('span');
+
+    card.appendChild(headLine);
+    card.appendChild(author);
+    author.appendChild(imgCont);
+    imgCont.appendChild(img);
+    author.appendChild(authorName);
+
+    card.classList.add('card');
+    headLine.classList.add('headline');
+    author.classList.add('author');
+    imgCont.classList.add('img-container');
+
+    img.src = data.authorPhoto;
+    
+//       <img src={url of authors image} />
+//     </div>
+//     <span>By {authors name}</span>
+//   </div>
+// </div>
+    
+
+
+}
